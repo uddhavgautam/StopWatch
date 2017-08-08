ddf1d84d96bfb6c5e2451520b01524d4f0164059
uddhav <upgautam@ualr.edu>
2017-08-08 03:55:25 -0500

    Completed Everything

--- app/src/main/java/com/example/uddhav/stopwatch/View/Activity/MainActivity.java
+++ app/src/main/java/com/example/uddhav/stopwatch/View/Activity/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.uddhav.stopwatch.View.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.example.uddhav.stopwatch.R;
 import com.example.uddhav.stopwatch.View.Fragment.StopWatchAddRemoveFragment;
@@ -15,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        ((TextView) findViewById(R.id.main_toolbar_title)).setText("Stop-Watch!");
+
+
+
         //start fragments
         if (savedInstanceState == null) {
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction(); //common transaction object
@@ -55,6 +70,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.stop_watch_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.AboutID:
+                Intent intent2 = new Intent(this, AboutActivity.class);
+                startActivity(intent2);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
 }
